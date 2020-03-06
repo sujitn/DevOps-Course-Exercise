1191055e7567da6223ba022232a11084ec0e51d9
Tom Bell <tom.bell@softwire.com>
2020-03-06 17:04:33 +0100

    Update Gunicorn configuration to be suitable for running in containers
    This is largely based on the recommendations outlined here:
    https://pythonspeed.com/articles/gunicorn-in-docker/

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,9 @@
-import multiprocessing
-
 bind = "0.0.0.0:80"
-workers = multiprocessing.cpu_count() * 2 + 1
+
+threads = 4
+workers = 2
+worker_class = "gthread"
+
+# Use an in-memory filesystem to store heartbeat files
+# For details, see: http://docs.gunicorn.org/en/stable/faq.html#how-do-i-avoid-gunicorn-excessively-blocking-in-os-fchmod
+worker_tmp_dir = "/dev/shm"
