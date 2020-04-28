b13bce23a488e53af4dc01ef9c63e1bc208383ba
Tom Bell <tom.bell@softwire.com>
2020-04-28 17:49:33 +0200

    Allow items to be started before being completed

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ def add_item():
     trello.add_item(name)
     return redirect(url_for('index')) 
 
+@app.route('/items/<id>/start')
+def start_item(id):
+    trello.start_item(id)
+    return redirect(url_for('index')) 
+
 @app.route('/items/<id>/complete')
 def complete_item(id):
     trello.complete_item(id)
