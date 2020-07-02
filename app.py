b34546b9e5321b13ec63e219aa6d7718c00102aa
sujitn <sujitnair@msn.com>
2020-07-02 16:38:10 +0100

    module 2 checkins

--- app.py
+++ app.py
@@ -1,23 +1,64 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+from item import TrelloItem
+from trello import TrelloClient
+import os
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+client = TrelloClient(
+    api_key=os.environ.get('TRELLO_API_KEY'),
+    token=os.environ.get('TRELLO_TOKEN')
+)
+
+
+board = client.get_board(os.environ.get('TRELLO_BOARD_ID'))
+
 @app.route('/')
 def index():
-    items = session.get_items()
+    
+    items = []
+
+    board_lists = board.list_lists()
+    for list_item in board_lists:
+        for card in list_item.list_cards():
+            test = TrelloItem(card.id, list_item.name,card.name)
+            items.append(TrelloItem(card.id, list_item.name,card.name))
+    
     return render_template('index.html', items = items)
 
 @app.route('/items/new', methods=['POST'])
 def add_item():
     title = request.form['title']
-    session.add_item(title)
+    
+    board_lists = board.list_lists()
+    todo_list = next((x for x in board_lists if x.name == "ToDo"), None)
+
+    todo_list.add_card(title);  
     return redirect(url_for('index')) 
 
 @app.route('/items/<id>/complete')
 def complete_item(id):
-    session.complete_item(id)
+
+    board_lists = board.list_lists()
+    todo_list = next((x for x in board_lists if x.name == "ToDo"), None)
+    done_list = next((x for x in board_lists if x.name == "Done"), None)
+    
+    cards = todo_list.list_cards()
+    card = next((x for x in cards if x.id == id), None)   
+    card.change_list(done_list.id)  
+    return redirect(url_for('index')) 
+    
+@app.route('/items/<id>/uncomplete')
+def uncomplete_item(id):
+    board_lists = board.list_lists()
+    todo_list = next((x for x in board_lists if x.name == "ToDo"), None)
+    done_list = next((x for x in board_lists if x.name == "Done"), None)
+    
+    cards = done_list.list_cards()
+    card = next((x for x in cards if x.id == id), None)   
+    card.change_list(todo_list.id)  
     return redirect(url_for('index')) 
 
 
---
+++ item.py
@@ -0,0 +1,5 @@
+class TrelloItem:
+	def __init__(self, id, status, title):
+		self.id = id
+		self.title = title
+		self.status = status
