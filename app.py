9e8fd035a086a321d42030c271d7d4d29338b826
Robin Leach <robin.leach@softwire.com>
2020-07-16 14:10:41 +0100

    fix lint errors

--- app.py
+++ app.py
@@ -14,26 +14,30 @@ handler = logging.StreamHandler(sys.stdout)
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.DEBUG)
 
+
 @app.route('/')
 def index():
     trello_lists = get_lists_on_board()
     trello_items = get_items_on_board()
-    
+
     items = map_trello_items(trello_lists, trello_items)
-    return render_template('index.html', items = items)
+    return render_template('index.html', items=items)
+
 
 @app.route('/items/<id>/complete')
 def complete_item(id):
     done_list_id = get_id_of_list(ListName.Done.value)
     update_item_list(id, done_list_id)
-    return redirect(url_for('index')) 
+    return redirect(url_for('index'))
+
 
 @app.route('/items/new', methods=[HttpMethod.Post.value])
 def add_item():
     title = request.form['title']
     to_do_list_id = get_id_of_list(ListName.ToDo.value)
     create_item(title, to_do_list_id)
-    return redirect(url_for('index')) 
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
